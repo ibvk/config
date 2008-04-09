399a534521a383454aaa1a280594cae867f0d103
BVK Chaitanya <bvk.chaitanya@yahoo.com>
2008-04-09 11:35:56 +0530

    enabled iswitchb for switching buffers

--- emacs/emacs.el
+++ emacs/emacs.el
@@ -44,6 +44,7 @@
 (require 'uniquify)
 (setq uniquify-buffer-name-style 'forward)
 
+(load "~/config/emacs/my-iswitchb.el")
 (load "~/config/emacs/bvk.el")
 (load "~/config/emacs/tramp.el")
 (load "~/config/emacs/my-elisp/mail-config.el")
