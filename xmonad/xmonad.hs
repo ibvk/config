a161b20fad99103755ac1b36583a66d27d3ad650
bvk@jeos <bvk@jeos>
2008-05-17 12:00:07 +0530

    update xmonad.hs to use tabbed layout

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,5 +1,11 @@
 import XMonad
+import XMonad.Layout.Tabbed
+import XMonad.Layout.NoBorders
+import XMonad.Layout.Roledex
+
+myLayouts = noBorders (tabbed shrinkText defaultTConf)
 
 main = xmonad defaultConfig {
+       	      layoutHook = myLayouts,
        	      modMask = mod4Mask
        }
