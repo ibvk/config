3a31bc853f8487caf8cb2b6414a095030bc6217b
bvk@debook.vxindia.veritas.com <bvk@debook.vxindia.veritas.com>
2008-08-13 17:57:02 +0530

    moved dzen2 privilege from root to local

--- scripts/status.sh
+++ scripts/status.sh
@@ -2,17 +2,23 @@
 
 while true
 do
-    DATE=`date +'%a %b %d %H:%M'`
+    STATUS=
+
+    DATE=`date +'%A %b %d %H:%M'`
+    test -n "$DATE" && STATUS="$DATE $STATUS"
 
     HDDTEMP=`hddtemp /dev/sda | cut -d' ' -f3 | sed -e 's/[^0-9\.]//g'`
+    test -n "$HDDTEMP" && STATUS="HDD $HDDTEMP $STATUS"
 
     FAN=`cat /sys/devices/platform/applesmc*/fan1_input`
+    test -n "$FAN" && STATUS="FAN $FAN $STATUS"
 
     TEMP=`sensors | cut -s -d\+ -f2 | sort | tail -n 1 | sed -e 's/[^0-9\.]//g'`
+    test -n "$TEMP" && STATUS="BOARD $TEMP $STATUS"
 
     ETH0=`/sbin/ifconfig eth0|grep 'addr:[0-9]'|sed 's/^.*addr\:\([^ ]*\).*$/\1/'`
+    test -n "$ETH0" && STATUS="ETH $ETH0 $STATUS"
 
-    echo ETH0 $ETH0 FAN $FAN HDD ${HDDTEMP}C CPU ${TEMP}C $DATE ' '
-
+    echo "$STATUS"
     sleep 5
-done | dzen2 -fn fixed -bg '#3f3f3f' -fg '#dcdcdc' -ta r
+done
