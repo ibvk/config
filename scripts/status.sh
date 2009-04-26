49a9cb39dfab0fbd91ff8891a55d08a766b77c6e
BVK Chaitanya <bvk.groups@gmail.com>
2009-04-26 19:27:38 +0530

    made status.sh script better

--- scripts/status.sh
+++ scripts/status.sh
@@ -1,27 +1,32 @@
 #!/bin/sh
 
-while true
-do
-    STATUS=
+STATUS=
+# CELCIUS="\0342\0204\0203"
+APPLESMC=`echo /sys/devices/platform/applesmc.???`
 
-    DATE=`date +'%A %b %d %l:%M'`
-    test -n "$DATE" && STATUS="$DATE $STATUS"
+DATE=`date +'%A %b %d %l:%M'`
+test -n "$DATE" && STATUS="$DATE "
 
-    HDDTEMP=`hddtemp /dev/sda | cut -d' ' -f3 | sed -e 's/[^0-9\.]//g'`
-    test -n "$HDDTEMP" && STATUS="HDD $HDDTEMP $STATUS"
+HDDTEMP=`hddtemp /dev/sda | cut -d' ' -f3 | sed -e 's/[^0-9\.]//g'`
+test -n "$HDDTEMP" && STATUS="HDD: $HDDTEMP$CELCIUS - $STATUS"
 
-    FAN=`cat /sys/devices/platform/applesmc*/fan1_input`
-    test -n "$FAN" && STATUS="FAN $FAN $STATUS"
+TEMP=`cat $APPLESMC/temp* | sort | tail -n 1 | cut -b1,2`
+test -n "$TEMP" && STATUS="CPU: $TEMP$CELCIUS - $STATUS"
 
-    TEMP=`sensors | cut -s -d\+ -f2 | sort | tail -n 1 | sed -e 's/[^0-9\.]//g'`
-    test -n "$TEMP" && STATUS="BOARD $TEMP $STATUS"
+FAN=`cat $APPLESMC/fan1_input`
+test -n "$FAN" && STATUS="FAN: $FAN - $STATUS"
 
-    ETH0=`/sbin/ifconfig eth0|grep 'addr:[0-9]'|sed 's/^.*addr\:\([^ ]*\).*$/\1/'`
-    test -n "$ETH0" && STATUS="ETH $ETH0 $STATUS"
+ETH0=`/sbin/ifconfig eth0|grep 'addr:[0-9]'|sed 's/^.*addr\:\([^ ]*\).*$/\1/'`
+test -n "$ETH0" && STATUS="ETH0: $ETH0 - $STATUS"
 
-    WLAN0=`/sbin/ifconfig wlan0|grep 'addr:[0-9]'|sed 's/^.*addr\:\([^ ]*\).*$/\1/'`
-    test -n "$WLAN0" && STATUS="WLAN $WLAN0 $STATUS"
+WLAN0=`/sbin/ifconfig wlan0|grep 'addr:[0-9]'|sed 's/^.*addr\:\([^ ]*\).*$/\1/'`
+test -n "$WLAN0" && STATUS="WLAN0: $WLAN0 - $STATUS"
 
-    echo "$STATUS"
-    sleep 5
-done
+BATT=`acpi -b | cut -d, -f2`
+test -n "$BATT" && STATUS="Battery:$BATT - $STATUS"
+
+POWR=`acpi -a | cut -d: -f2 | cut -d- -f1 | tr [:lower:] [:upper:]`
+test -n "$POWR" && STATUS="Power:$POWR - $STATUS"
+
+echo -e "$STATUS"
+
